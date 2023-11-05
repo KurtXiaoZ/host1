a6338834eba105e27efb80e18313acc1d150af87
KurtXiaoZ <kurtxiaoz@gmail.com>
2023-11-05 14:44:38 -0500

    add github pages and move away from nextjs

--- src/app/page.jsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import { MaterialIconPicker } from "react-material-icon-picker";
-import styles from './page.module.css';
-
-export default function Home() {
-  console.log('render Home');
-  return <div className={styles.container}><MaterialIconPicker /></div>
-}
